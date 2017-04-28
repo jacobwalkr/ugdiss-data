6f9b57d5818713fd2e589af8f6725ced497d61a2
Jacob Walker <jacob.walker94@gmail.com>
2017-04-28 17:26:08 +0100

    Add localise_robotarena_naive.py

---
+++ localise_robotarena_naive.py
@@ -0,0 +1,57 @@
+import sys
+import os
+import mflog_utils
+import numpy as np
+import tabulate
+
+#=================================================================================================#
+# Prepare "training" data. Not strictly "training" data but I'm not sure what else to call it     #
+#=================================================================================================#
+training_folders = ['RobotArena/0/', 'RobotArena/1/']
+training_files = []
+
+for training_folder in training_folders:
+    training_files += [training_folder + f for f in os.listdir(training_folder)
+        if os.path.isfile(os.path.join(training_folder, f))]
+
+labels = [] # plain python list
+arg_labels = np.empty(len(training_files), dtype=np.int32) # medium for argsorting labels
+training_data = np.zeros((len(training_folders) * 16, 3))
+
+for i, filename in enumerate(training_files):
+    summary = mflog_utils.summarise_file(filename)
+    labels.append(summary['location'])
+    arg_labels[i] = i
+    xm = summary['x']['median']
+    ym = summary['y']['median']
+    zm = summary['z']['median']
+    training_data[i, :] = [xm, ym, zm]
+
+#=================================================================================================#
+# Run through test files and compare them to "training" data                                      #
+#=================================================================================================#
+test_folder = 'RobotArena/2/'
+test_files = [test_folder + f for f in os.listdir(test_folder)
+    if os.path.isfile(os.path.join(test_folder, f))]
+
+results = []
+
+for i, filename in enumerate(test_files):
+    test_summary = mflog_utils.summarise_file(filename)
+    test_location = test_summary['location']
+    test_data = np.array([
+        test_summary['x']['median'],
+        test_summary['y']['median'],
+        test_summary['z']['median'],
+    ])
+
+    # vectors representing Euclidean distance to test point
+    distances = np.linalg.norm(training_data - test_data, axis=1)
+
+    # get the closest point and find its label
+    min_idx = np.argmin(distances)
+    result_location = labels[arg_labels[min_idx]]
+    results.append((test_location, result_location))
+
+#print('\n'.join([test + ';' + result for test, result in results]))
+print(tabulate.tabulate(results, headers=('Test location', 'Closest match')))
