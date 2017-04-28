1d812f3aa2d91251da95795ddb4d91e2ca70f232
Jacob Walker <jacob.walker94@gmail.com>
2017-04-28 13:47:46 +0100

    Find data files for robotarena_cluster.py in script

--- robotarena_cluster.py
+++ robotarena_cluster.py
@@ -1,3 +1,4 @@
+import sys
 import os
 from itertools import cycle
 import matplotlib as mpl
@@ -17,16 +18,24 @@ ax.set_zlabel('Z', size='xx-large')
 summaries = {}
 
 # Read in input files
-for dirpath, _, filenames in os.walk('RobotArena'):
-    for file in filenames:
-        summary = mflog_utils.summarise_file(os.path.join(dirpath, file))
-        coords = summary['location']
+run_numbers = [int(num) for num in sys.argv[1:-1]]
 
-        #print(summaries)
-        if coords in summaries:
-            summaries[coords].append(summary)
-        else:
-            summaries[coords] = [summary]
+for run_number in run_numbers:
+    for dirpath, _, filenames in os.walk('RobotArena/' + str(run_number)):
+        # Yay, order!
+        print('Doing run #' + str(run_number))
+
+        for file in filenames:
+            summary = mflog_utils.summarise_file(os.path.join(dirpath, file))
+            coords = summary['location']
+
+            if coords in summaries:
+                summaries[coords].append(summary)
+            else:
+                summaries[coords] = [summary]
+
+        # just the once - no subdirs
+        break
 
 colours = cycle(mflog_utils.tableau20)
 for location in summaries.values():
@@ -41,5 +50,6 @@ for location in summaries.values():
     ax.plot(*zip(*location_points), color=colour)
 
 fig.tight_layout()
-plt.savefig('Plots/RobotArena012_scatter.png')
+plt.savefig(sys.argv[-1] + '.png')
+# TODO: print metadata. Will be the same as a diff plot with the same run numbers
 plt.show()
